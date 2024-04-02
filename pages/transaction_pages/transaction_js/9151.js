224796e49831c85b7f6fb44a4ee4eb142c79620c
MD Saydur Rahman <136433372+sr-official@users.noreply.github.com>
2024-04-02 22:13:28 +0600

    Update 9151.js
    50 deducted from Abrar

--- pages/transaction_pages/transaction_js/9151.js
+++ pages/transaction_pages/transaction_js/9151.js
@@ -1,6 +1,6 @@
 // Sample student transaction data
 const studentTransactions = [
-	{ depositDate: '16-03-2024', depositAmount: 50, deductDate: '00-00-0000', deductedAmount: '00' },
+	{ depositDate: '16-03-2024', depositAmount: 50, deductDate: '01-04-2024', deductedAmount: 50 },
 	{ depositDate: '00-00-0000', depositAmount: '00', deductDate: '00-00-0000', deductedAmount: '00' },
 	// Add more student transaction data as needed
 ];
